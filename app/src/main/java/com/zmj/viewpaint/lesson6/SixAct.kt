89e5abcd8cab68fd59bdf591ced29bdc7fa1252d
ZmjDns <292570927@qq.com>
2020-04-29 22:46:26 +0800

    showView

---
+++ app/src/main/java/com/zmj/viewpaint/lesson6/SixAct.kt
@@ -0,0 +1,21 @@
+package com.zmj.viewpaint.lesson6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/4/28
+ * Description :
+ */
+class SixAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_six)
+
+    }
+}
