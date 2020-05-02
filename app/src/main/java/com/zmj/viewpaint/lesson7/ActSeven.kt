db1e2d5dbbd3f391924108823fd0ff47e06030ba
ZmjDns <292570927@qq.com>
2020-05-02 22:32:54 +0800

    showView

---
+++ app/src/main/java/com/zmj/viewpaint/lesson7/ActSeven.kt
@@ -0,0 +1,23 @@
+package com.zmj.viewpaint.lesson7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/1
+ * Description :
+ */
+class ActSeven:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_seven)
+
+
+    }
+}
