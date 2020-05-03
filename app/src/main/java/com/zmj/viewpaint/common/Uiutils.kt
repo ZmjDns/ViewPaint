203db24e09d4cf716332b3be5ecd7b5f58aac2e3
ZmjDns <292570927@qq.com>
2020-05-03 15:59:59 +0800

    图片获取

--- app/src/main/java/com/zmj/viewpaint/common/Uiutils.kt
+++ app/src/main/java/com/zmj/viewpaint/common/Uiutils.kt
@@ -1,7 +1,11 @@
 package com.zmj.viewpaint.common
 
+import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.TypedValue
+import com.zmj.viewpaint.R
 
 /**
  * Author : Zmj
@@ -13,4 +17,17 @@ import android.util.TypedValue
 
 fun dp2px(dp: Float): Float{
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,Resources.getSystem().displayMetrics)
+}
+
+/**
+ * 获取图片Bitmap
+ */
+fun getAvatarBit(resources: Resources,width: Float): Bitmap{
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true   //设置以后，在options中只会取到图片的宽高，得到宽高比之后可以计算实际需要显示的宽高
+    BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+    options.inJustDecodeBounds = false
+    options.inDensity = options.outWidth
+    options.inTargetDensity = width.toInt()
+    return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
 }
