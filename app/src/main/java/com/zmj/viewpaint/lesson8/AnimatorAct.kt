844add049dab2497ab099eea26c2a7592dd32bd7
ZmjDns <292570927@qq.com>
2020-05-08 22:55:51 +0800

    通过动画将view展示出来

---
+++ app/src/main/java/com/zmj/viewpaint/lesson8/AnimatorAct.kt
@@ -0,0 +1,58 @@
+package com.zmj.viewpaint.lesson8
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+import com.zmj.viewpaint.common.dp2px
+import kotlinx.android.synthetic.main.act_eight.*
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/8
+ * Description :
+ */
+class AnimatorAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_eight)
+
+        initCircleView()
+
+        flipPageAnimator()
+    }
+
+    private fun initCircleView(){
+        val objectAnimator: ObjectAnimator = ObjectAnimator.ofFloat(circleView,"radius", dp2px(150f))
+        objectAnimator.startDelay = 1000
+        objectAnimator.start()
+    }
+
+    private fun flipPageAnimator() {
+        /*val objectAnimator = ObjectAnimator.ofFloat(flipPage,"bottomFlip",135f)
+        objectAnimator.duration = 1000
+        objectAnimator.startDelay = 1000
+        objectAnimator.start()*/
+
+        val bottomFlipAnimator = ObjectAnimator.ofFloat(flipPage,"bottomFlip",45f)
+        bottomFlipAnimator.duration = 1500
+
+
+        val rotationAnimator = ObjectAnimator.ofFloat(flipPage,"rotationCanvas",270f)
+        rotationAnimator.duration = 1500
+
+        val topFlipAnimator = ObjectAnimator.ofFloat(flipPage,"topFlip",-45f)
+        rotationAnimator.duration = 1500
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(bottomFlipAnimator,rotationAnimator,topFlipAnimator)
+        animatorSet.startDelay = 1000
+        animatorSet.start()
+
+    }
+}
