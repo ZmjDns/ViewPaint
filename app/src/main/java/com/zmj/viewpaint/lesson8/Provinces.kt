66043682acba24b5196d59315458d72e40fd7de2
ZmjDns <292570927@qq.com>
2020-05-14 22:40:37 +0800

    省市数据

---
+++ app/src/main/java/com/zmj/viewpaint/lesson8/Provinces.kt
@@ -0,0 +1,46 @@
+package com.zmj.viewpaint.lesson8
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/11
+ * Description :
+ */
+object Provinces {
+    val provinces = arrayOf("北京",
+        "天津",
+        "河北",
+        "山西",
+        "内蒙古",
+        "辽宁",
+        "吉林",
+        "黑龙江",
+        "上海",
+        "江苏",
+        "浙江省",
+        "安徽",
+        "福建",
+        "江西",
+        "山东",
+        "河南",
+        "湖北",
+        "湖南",
+        "广东",
+        "广西",
+        "海南",
+        "重庆",
+        "四川",
+        "贵州",
+        "云南",
+        "西藏",
+        "陕西",
+        "甘肃省",
+        "青海",
+        "宁夏",
+        "新疆",
+        "台湾",
+        "香港特别行政区",
+        "澳门特别行政区"
+    )
+}
