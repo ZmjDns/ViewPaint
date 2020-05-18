32e2f6998e4d1cb7311f78622a230b7061c526c7
ZmjDns <292570927@qq.com>
2020-05-18 22:55:29 +0800

    TagLayout

---
+++ app/src/main/java/com/zmj/viewpaint/lesson10/TagLayout.kt
@@ -0,0 +1,19 @@
+package com.zmj.viewpaint.lesson10
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/18
+ * Description :
+ */
+class TagLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+    }
+}
