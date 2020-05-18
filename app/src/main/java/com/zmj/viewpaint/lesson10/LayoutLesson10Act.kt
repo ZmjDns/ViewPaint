ddfff1de24ef908474ac848049b51ca4800aa946
ZmjDns <292570927@qq.com>
2020-05-18 22:55:51 +0800

    show

---
+++ app/src/main/java/com/zmj/viewpaint/lesson10/LayoutLesson10Act.kt
@@ -0,0 +1,21 @@
+package com.zmj.viewpaint.lesson10
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/17
+ * Description :
+ */
+class LayoutLesson10Act: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_layout)
+    }
+}
