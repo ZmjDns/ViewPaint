e7d1560aae7a19ff59b4f966f056903a46a1825f
ZmjDns <292570927@qq.com>
2020-05-24 22:35:55 +0800

    添加放大之后的偏移，惯性滑动动画设置

--- app/src/main/java/com/zmj/viewpaint/lesson12/ScalableImageView.kt
+++ app/src/main/java/com/zmj/viewpaint/lesson12/ScalableImageView.kt
@@ -9,6 +9,8 @@ import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.OverScroller
+import android.widget.Scroller
 import androidx.core.view.GestureDetectorCompat
 import com.zmj.viewpaint.common.dp2px
 import com.zmj.viewpaint.common.getAvatarBit
@@ -21,14 +23,14 @@ import com.zmj.viewpaint.common.getAvatarBit
  * Description :可缩放ImageView
  */
 class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs),GestureDetector.OnGestureListener,
-    GestureDetector.OnDoubleTapListener {
+    GestureDetector.OnDoubleTapListener, Runnable {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val BIT_WIDTH = dp2px(200f)
     private val OVER_SCALE_FACTOR = 1.5f        //放大系数
     private var bitmap: Bitmap = getAvatarBit(resources,BIT_WIDTH)
-    private var offSetX:Float = 0f
-    private var offSetY: Float = 0f
+    private var originalOffSetX:Float = 0f
+    private var originalOffSetY: Float = 0f
     private var smallScale: Float = 0f
     private var bigScale: Float = 0f
     private var big: Boolean = false
@@ -41,6 +43,10 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
         }
     private val scaleAnimator by lazy { ObjectAnimator.ofFloat(this,"scaleFraction",0f,1f) }
 
+    private var offsetX = 0f
+    private var offsetY = 0f
+    private val scroller = OverScroller(context)
+
     private var decetor: GestureDetectorCompat = GestureDetectorCompat(context,this)
     /*private val decteor2 = GestureDetectorCompat(context,object :GestureDetector.SimpleOnGestureListener(){
         override fun onDoubleTap(e: MotionEvent?): Boolean {
@@ -52,8 +58,8 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        offSetX = (width.toFloat() - bitmap.width) / 2
-        offSetY = (height.toFloat() - bitmap.height) / 2
+        originalOffSetX = (width.toFloat() - bitmap.width) / 2
+        originalOffSetY = (height.toFloat() - bitmap.height) / 2
 
         if ((bitmap.width/bitmap.height).toFloat() > (width/height).toFloat()){
             smallScale = width.toFloat()/bitmap.width
@@ -69,12 +75,16 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
     }
 
     override fun onDraw(canvas: Canvas?) {
+        //canvas执行顺序是倒序的
         super.onDraw(canvas)
+
+        //缩放之后的第二次偏移
+        canvas!!.translate(offsetX,offsetY)
 
         //val scale = if (big) bigScale else smallScale
         val scale = smallScale + (bigScale - smallScale) * scaleFraction
-        canvas!!.scale(scale,scale,width/2f,height/2f)
-        canvas.drawBitmap(bitmap,offSetX,offSetY,paint)
+        canvas.scale(scale,scale,width/2f,height/2f)
+        canvas.drawBitmap(bitmap,originalOffSetX,originalOffSetY,paint)
     }
 
     //所有事件的执行的入口，
@@ -98,11 +108,20 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
 
     //onMove
     override fun onScroll(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        distanceX: Float,
-        distanceY: Float
+        down: MotionEvent?,     //按下事件
+        event: MotionEvent?,    //当前事件
+        distanceY: Float,       //当前点距离上一个事件X方向上的偏移  注意   旧位置 - 新位置
+        distanceX: Float       //当前点距离上一个事件Y方向上的偏移   注意   旧位置 - 新位置
     ): Boolean {
+        if (big){
+            offsetX -= distanceX        //注意  distanceX = 旧位置X - 新位置X           （画图计算）
+            offsetX = Math.min(offsetX,((bitmap.width * bigScale - width)/2 ))
+            offsetX = Math.max(offsetX,-((bitmap.width * bigScale - width)/2))
+            offsetY -= distanceY
+            offsetY = Math.min(offsetY,((bitmap.height * bigScale - height)/2))
+            offsetY = Math.max(offsetY,-((bitmap.height * bigScale - height)/2))
+            invalidate()
+        }
         return false
     }
 
@@ -110,12 +129,31 @@ class ScalableImageView(context: Context?, attrs: AttributeSet?) : View(context,
 
     }
     override fun onFling(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
+        down: MotionEvent?,
+        event: MotionEvent?,
         velocityX: Float,
         velocityY: Float
     ): Boolean {
+        if (big){
+            scroller.fling(offsetX.toInt(),offsetY.toInt(),velocityX.toInt(),velocityY.toInt(),
+            -((bitmap.width * bigScale - width)/2).toInt(),
+            ((bitmap.width * bigScale - width)/2).toInt(),
+            -((bitmap.height * bigScale - height)/2).toInt(),
+            ((bitmap.height * bigScale - height)/2).toInt()
+                /*,100,100*/)//最后两个参数，过度滚动的位置
+            //下一帧执行run方法中的代码
+            postOnAnimation(this)
+        }
         return false
+    }
+    override fun run() {
+        if (scroller.computeScrollOffset()){    //测量动画是否完成
+            offsetX = scroller.currX.toFloat()
+            offsetY = scroller.currY.toFloat()
+            invalidate()    //此时只会刷新一次，要将动画执行完可能会需要执行跟多次刷新
+            //一次在动画没有执行完毕的时候需要再次执行 postOnAnimation(this)
+            postOnAnimation(this)
+        }
     }
 
     //确认单击而不是双击
