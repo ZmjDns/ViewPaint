49c610c0af7bf348c52246cb4240c17240dae14c
ZmjDns <292570927@qq.com>
2020-05-26 22:53:19 +0800

    优化

---
+++ app/src/main/java/com/zmj/viewpaint/lesson12/Lesson12Act.kt
@@ -0,0 +1,21 @@
+package com.zmj.viewpaint.lesson12
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/5/22
+ * Description :
+ */
+class Lesson12Act: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_lesson12)
+    }
+}
