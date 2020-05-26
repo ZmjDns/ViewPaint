79d35370c6357f09699f0b1a28d8efe197ddbb81
ZmjDns <292570927@qq.com>
2020-05-26 22:49:15 +0800

    practice finished

--- app/src/main/java/com/zmj/viewpaint/lesson12/ScalablePracticeView.kt
+++ app/src/main/java/com/zmj/viewpaint/lesson12/ScalablePracticeView.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.OverScroller
 import androidx.core.view.GestureDetectorCompat
 import com.zmj.viewpaint.common.dp2px
 import com.zmj.viewpaint.common.getAvatarBit
@@ -23,7 +24,7 @@ import kotlin.properties.Delegates
  * Description :
  */
 class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(context, attrs),
-    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, Runnable {
 
     private val TAG = this.javaClass.name
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -31,6 +32,9 @@ class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(conte
 
     private var originalOffsetX = 0f
     private var originalOffsetY = 0f
+
+    private var offSetX = 0f
+    private var offSetY = 0f
 
     private var smallScale: Float = 0f
     private var bigScale: Float = 0f
@@ -42,16 +46,18 @@ class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(conte
         get() = field
         set(value) {
             field = value
+            invalidate()
         }
 
-    private val animator = ObjectAnimator.ofFloat(this,"fraction",0f,1f)
-
+    private val animator by lazy {ObjectAnimator.ofFloat(this,"fraction",0f,1f)}
 
     /*init {
         detector.setOnDoubleTapListener(this)
     }*/
 
     private var big = false
+
+    private val scroller by lazy { OverScroller(context) }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -77,11 +83,13 @@ class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(conte
         super.onDraw(canvas!!)
 
 
+        canvas.translate(offSetX,offSetY)
+
         val scale= smallScale + (bigScale - smallScale) * fraction
-        canvas.save()
+        //canvas.save()
         canvas.scale(scale,scale,width/2f,height/2f)//
         canvas.drawBitmap(bitmap,originalOffsetX,originalOffsetY,paint)
-        canvas.restore()
+        //canvas.restore()
     }
 
     override fun onDown(e: MotionEvent?): Boolean {
@@ -94,23 +102,48 @@ class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(conte
         return false
     }
 
-    override fun onFling(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        return false
-    }
-
     override fun onScroll(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
+        down: MotionEvent?,
+        event: MotionEvent?,
         distanceX: Float,
         distanceY: Float
     ): Boolean {
+        if(big){
+            offSetX -= distanceX
+            offSetX = Math.min(offSetX,(bitmap.width*bigScale-width)/2)
+            offSetX = Math.max(offSetX,-(bitmap.width*bigScale-width)/2)
+            offSetY -= distanceY
+            offSetY = Math.min(offSetY,(bitmap.height*bigScale -height)/2)
+            offSetY = Math.max(offSetY,-(bitmap.height*bigScale -height)/2)
+            invalidate()
+        }
         return false
     }
+
+    override fun onFling(
+        down: MotionEvent?,
+        event: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        scroller.fling(offSetX.toInt(),offSetY.toInt(),velocityX.toInt(),velocityY.toInt(),
+            -((bitmap.width*bigScale-width)/2).toInt(),((bitmap.width*bigScale-width)/2).toInt(),
+        -((bitmap.height*bigScale-height)/2).toInt(),((bitmap.height*bigScale-height)/2).toInt())
+
+        postOnAnimation(this)
+        return false
+    }
+
+    override fun run() {
+        if (scroller.computeScrollOffset()){
+            offSetX = scroller.currX.toFloat()
+            offSetY = scroller.currY.toFloat()
+            invalidate()
+
+            postOnAnimation(this)
+        }
+    }
+
 
     override fun onLongPress(e: MotionEvent?) {
 
@@ -120,9 +153,9 @@ class ScalablePracticeView(context: Context?, attrs: AttributeSet?) : View(conte
         Log.i(TAG,"双击了.....")
         big = !big
         if(big){
-            animator.reverse()
-        }else{
             animator.start()
+        }else{
+            animator.reverse()
         }
         //invalidate()
         return false
