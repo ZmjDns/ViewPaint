8707f38bc515176ed33725ec5c447c2d2577391f
ZmjDns <292570927@qq.com>
2020-05-28 17:43:28 +0800

    整合统一入口

--- app/src/main/java/com/zmj/viewpaint/MainActivity.kt
+++ app/src/main/java/com/zmj/viewpaint/MainActivity.kt
@@ -3,8 +3,13 @@ package com.zmj.viewpaint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmj.viewpaint.lesson10.LayoutLesson10Act
+import com.zmj.viewpaint.lesson11.Lesson11Act
 import com.zmj.viewpaint.lesson12.Lesson12Act
 import com.zmj.viewpaint.lesson6.SixAct
+import com.zmj.viewpaint.lesson7.ActSeven
+import com.zmj.viewpaint.lesson8.AnimatorAct
+import com.zmj.viewpaint.lesson9.BitmapDrawableAct
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +24,21 @@ class MainActivity : AppCompatActivity() {
         lesson6.setOnClickListener {
             startActivity(Intent(this,SixAct::class.java))
         }
-
+        lesson7.setOnClickListener {
+            startActivity(Intent(this, ActSeven::class.java))
+        }
+        lesson8.setOnClickListener {
+            startActivity(Intent(this, AnimatorAct::class.java))
+        }
+        lesson9.setOnClickListener {
+            startActivity(Intent(this, BitmapDrawableAct::class.java))
+        }
+        lesson10.setOnClickListener {
+            startActivity(Intent(this, LayoutLesson10Act::class.java))
+        }
+        lesson11.setOnClickListener {
+            startActivity(Intent(this, Lesson11Act::class.java))
+        }
         lesson12.setOnClickListener {
             startActivity(Intent(this,Lesson12Act::class.java))
         }
