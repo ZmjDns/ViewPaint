6d4126f42cf6f43ea812bd45a788536ec778997b
ZmjDns <292570927@qq.com>
2020-06-10 22:54:02 +0800

    show

---
+++ app/src/main/java/com/zmj/viewpaint/lesson13_multi_touch/MultiTouchAct.kt
@@ -0,0 +1,25 @@
+package com.zmj.viewpaint.lesson13_multi_touch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/6/7
+ * Description :
+ */
+class MultiTouchAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_multi_touch_13)
+
+
+
+    }
+
+}
