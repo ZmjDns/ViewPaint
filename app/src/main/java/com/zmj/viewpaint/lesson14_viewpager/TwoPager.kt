b3703f42519edadf89bb85c2b0583b7e710d2baa
ZmjDns <292570927@qq.com>
2020-06-13 22:52:58 +0800

    简单的ViewPager Begin

---
+++ app/src/main/java/com/zmj/viewpaint/lesson14_viewpager/TwoPager.kt
@@ -0,0 +1,20 @@
+package com.zmj.viewpaint.lesson14_viewpager
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/6/13
+ * Description : 简单的ViewPager
+ */
+class TwoPager(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+}
