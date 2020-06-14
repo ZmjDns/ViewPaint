74c432a7826b0a50ce20a04e91a8c63e175982fa
ZmjDns <292570927@qq.com>
2020-06-14 22:52:48 +0800

    simple config

--- app/src/main/java/com/zmj/viewpaint/lesson14_viewpager/TwoPager.kt
+++ app/src/main/java/com/zmj/viewpaint/lesson14_viewpager/TwoPager.kt
@@ -2,7 +2,11 @@ package com.zmj.viewpaint.lesson14_viewpager
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.VelocityTracker
+import android.view.ViewConfiguration
 import android.view.ViewGroup
+import android.widget.OverScroller
 
 /**
  * Author : Zmj
@@ -12,6 +16,31 @@ import android.view.ViewGroup
  * Description : 简单的ViewPager
  */
 class TwoPager(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+
+    private var minVelocity: Float
+    private var maxVelocity: Float
+    private val overScroller = OverScroller(context)
+    private var viewConfiguration: ViewConfiguration = ViewConfiguration.get(context)
+    private val velocityTracker = VelocityTracker.obtain()//速度追踪器
+
+    init {
+        minVelocity = viewConfiguration.scaledMinimumFlingVelocity.toFloat()
+        maxVelocity = viewConfiguration.scaledMaximumFlingVelocity.toFloat()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        measureChildren(widthMeasureSpec,heightMeasureSpec)
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.actionMasked == MotionEvent.ACTION_DOWN){
+            velocityTracker.clear()
+        }
+        velocityTracker.addMovement(ev)
+
+        return super.onInterceptTouchEvent(ev)
+    }
 
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
