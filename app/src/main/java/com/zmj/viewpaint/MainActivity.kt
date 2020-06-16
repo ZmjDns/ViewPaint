900966bb897b20efa018f0490becada27a548cd4
ZmjDns <292570927@qq.com>
2020-06-16 22:38:49 +0800

    设置沉浸式状态栏

--- app/src/main/java/com/zmj/viewpaint/MainActivity.kt
+++ app/src/main/java/com/zmj/viewpaint/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.zmj.viewpaint
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.zmj.viewpaint.lesson10.LayoutLesson10Act
 import com.zmj.viewpaint.lesson11.Lesson11Act
 import com.zmj.viewpaint.lesson12.Lesson12Act
+import com.zmj.viewpaint.lesson13_multi_touch.MultiTouchAct
+import com.zmj.viewpaint.lesson14_viewpager.Lesson14Act
 import com.zmj.viewpaint.lesson6.SixAct
 import com.zmj.viewpaint.lesson7.ActSeven
 import com.zmj.viewpaint.lesson8.AnimatorAct
@@ -17,6 +23,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            val decorView = window.decorView
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+//        toolBar.textAlignment = View.TEXT_ALIGNMENT_CENTER
+//        setSupportActionBar(toolBar)
+
         initView()
     }
 
@@ -41,6 +57,12 @@ class MainActivity : AppCompatActivity() {
         }
         lesson12.setOnClickListener {
             startActivity(Intent(this,Lesson12Act::class.java))
+        }
+        lesson13.setOnClickListener {
+            startActivity(Intent(this,MultiTouchAct::class.java))
+        }
+        lesson14.setOnClickListener {
+            startActivity(Intent(this,Lesson14Act::class.java))
         }
     }
 }
