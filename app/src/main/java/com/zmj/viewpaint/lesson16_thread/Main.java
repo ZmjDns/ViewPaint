9cd940659cdeef185a423cf69d6667ef6d51eed6
ZmjDns <292570927@qq.com>
2020-06-17 22:52:24 +0800

    线程相关

---
+++ app/src/main/java/com/zmj/viewpaint/lesson16_thread/Main.java
@@ -0,0 +1,77 @@
+package com.zmj.viewpaint.lesson16_thread;
+
+import android.util.Log;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/6/17
+ * Description :
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Main run");
+    }
+
+    private static void threadPool(){
+        BlockingQueue<Runnable> blockingQueue = new LinkedBlockingDeque<>(1000);
+        Executor threadPool = new ThreadPoolExecutor(5,100,5, TimeUnit.SECONDS,blockingQueue);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+        threadPool.execute(runnable);
+    }
+
+    private static void callable(){
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+
+                try{
+                    Thread.sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return "Done";
+            }
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Future<String> future = executor.submit(callable);
+
+        while (true){
+            if (future.isDone()){
+                try {
+                    String result = future.get();//阻塞线程
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
