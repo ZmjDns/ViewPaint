1b3d33000742d8c6399ba75f95816e67ff287a28
ZmjDns <292570927@qq.com>
2020-06-19 21:13:36 +0800

    act14

---
+++ app/src/main/java/com/zmj/viewpaint/lesson14_viewpager/Lesson14Act.kt
@@ -0,0 +1,20 @@
+package com.zmj.viewpaint.lesson14_viewpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/6/15
+ * Description :
+ */
+class Lesson14Act: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_14)
+    }
+}
