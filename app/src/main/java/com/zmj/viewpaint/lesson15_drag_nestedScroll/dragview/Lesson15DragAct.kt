7b39cc5abfe41dae61a9843f4e67ca3fdcf17fb0
ZmjDns <292570927@qq.com>
2020-07-01 22:27:50 +0800

    15drag

---
+++ app/src/main/java/com/zmj/viewpaint/lesson15_drag_nestedScroll/dragview/Lesson15DragAct.kt
@@ -0,0 +1,22 @@
+package com.zmj.viewpaint.lesson15_drag_nestedScroll.dragview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/6/28
+ * Description :
+ */
+class Lesson15DragAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_15_draglistener)
+
+    }
+}
