5ac37b5634bd925d034f95e95321707a00f6f1f5
ZmjDns <292570927@qq.com>
2020-07-04 21:32:58 +0800

    15

--- app/src/main/java/com/zmj/viewpaint/lesson15_drag_nestedScroll/dragview/Lesson15DragAct.kt
+++ app/src/main/java/com/zmj/viewpaint/lesson15_drag_nestedScroll/dragview/Lesson15DragAct.kt
@@ -16,7 +16,7 @@ class Lesson15DragAct: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.act_15_draglistener)
+        setContentView(R.layout.drag_up_down_layout)
 
     }
 }
