a17cfeef4f9deb3339428633bf75ee2d1838bfbf
ZmjDns <292570927@qq.com>
2020-07-07 22:24:38 +0800

    线程及锁相关

--- app/src/main/java/com/zmj/viewpaint/lesson16_thread/ThreadTest.java
+++ app/src/main/java/com/zmj/viewpaint/lesson16_thread/ThreadTest.java
@@ -13,6 +13,8 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Author : Zmj
@@ -22,6 +24,17 @@ import java.util.concurrent.TimeUnit;
  * Description :
  */
 public class ThreadTest {
+
+    /**
+     * volatile只对基本类型数据保持原子操作性，但是对变量自增不起作用（a++，a--等）
+     * 对对象的赋值具有原子操作性，但是对对象的属性不具有原子保护性
+     */
+    private volatile int a = 0; //让变量a同时具有原子性和同步性    volatile类似于一个小型的synchronized
+
+    /**
+     * Atomic+基本类型，保证变量自增的同步性（a++，a--等）
+     */
+    private AtomicInteger atomicInteger ;
 
     public static void main(String[] args){
 
@@ -75,11 +88,12 @@ public class ThreadTest {
 
     private void threadFactory(){
         ThreadFactory threadFactory = new ThreadFactory() {
-            int count = 0;
+            //int count = 0;
+            AtomicInteger count = new AtomicInteger(0); //保证线程安全
             @Override
             public Thread newThread(Runnable r) {
-                count++;
-                return new Thread(r,"Thrad-" + count);
+                //count++;
+                return new Thread(r,"Thrad-" + count.incrementAndGet());
             }
         };
 
@@ -91,8 +105,50 @@ public class ThreadTest {
         };
 
         Thread thread1 = threadFactory.newThread(runnable);
+    }
 
+    private void callableTest(){
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                //doSomeThing
+                return "done";
+            }
+        };
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<String> future = executor.submit(callable);
 
+        try {
+            String result = future.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //读写锁
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+    ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+
+    private int x = 0;
+    private int y = 0;
+    //改变数据的时候加写入锁，此时不允许读
+    private void count(int newValue){
+        writeLock.lock();
+        try {
+            x = newValue;
+            y = newValue;
+        }finally {
+            writeLock.unlock();
+        }
+    }
+    private void print(){
+        readLock.lock();
+        try{
+            System.out.println("x = " + x + "," + "y = " + y );
+        }finally {
+            readLock.unlock();
+        }
     }
 
 
