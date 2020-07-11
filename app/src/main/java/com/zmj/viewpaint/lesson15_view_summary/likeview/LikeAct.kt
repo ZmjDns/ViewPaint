04f5edd8aa2c56165d3ba0a27dd1f323fc55a02f
ZmjDns <292570927@qq.com>
2020-07-11 23:19:07 +0800

    LikeAct

---
+++ app/src/main/java/com/zmj/viewpaint/lesson15_view_summary/likeview/LikeAct.kt
@@ -0,0 +1,20 @@
+package com.zmj.viewpaint.lesson15_view_summary.likeview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zmj.viewpaint.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/7/9
+ * Description :
+ */
+class LikeAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_like)
+    }
+}
