91c987dad014beef01538c6f1fc21c69cc9d31e3
ZmjDns <292570927@qq.com>
2020-07-22 15:49:31 +0800

    尺子滑动回调

---
+++ app/src/main/java/com/zmj/viewpaint/lesson15_view_summary/rulerview/RulerCallBack.java
@@ -0,0 +1,13 @@
+package com.zmj.viewpaint.lesson15_view_summary.rulerview;
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/7/15
+ * Description :
+ */
+public interface RulerCallBack {
+    //选取刻度变化时回调
+    void onScaleChanging(float scale);
+}
