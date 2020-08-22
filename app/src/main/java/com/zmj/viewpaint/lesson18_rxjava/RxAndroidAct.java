76947bfe283f3cb289dc578f6a9e420f50388cee
ZmjDns <292570927@qq.com>
2020-08-22 21:25:46 +0800

    rxjava

--- app/src/main/java/com/zmj/viewpaint/lesson18_rxjava/RxAndroidAct.java
+++ app/src/main/java/com/zmj/viewpaint/lesson18_rxjava/RxAndroidAct.java
@@ -8,9 +8,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zmj.viewpaint.R;
+import com.zmj.viewpaint.lesson18_rxjava.model.BaseResp;
+import com.zmj.viewpaint.lesson18_rxjava.model.Data;
+import com.zmj.viewpaint.lesson18_rxjava.model.DetailResp;
 import com.zmj.viewpaint.lesson18_rxjava.model.Repo;
 import com.zmj.viewpaint.lesson18_rxjava.net.Api;
+import com.zmj.viewpaint.lesson18_rxjava.net.GongShangApi;
+import com.zmj.viewpaint.lesson18_rxjava.net.ServiceCreator;
 
+import java.io.IOException;
 import java.util.List;
 
 import io.reactivex.Observable;
@@ -21,6 +27,10 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -138,9 +148,57 @@ public class RxAndroidAct extends AppCompatActivity {
                     }
                 });
 
+        getBaseData();
 
     }
 
+
+    private void getBaseData(){
+        Data data = new Data("天津君腾跃飞工程有限公司");   //天津天空地海科技有限公司  //天津君腾跃飞工程有限公司  //天津云科世讯科技
+        final GongShangApi gongShangApi = ServiceCreator.INSTANCE.create(GongShangApi.class);
+
+        gongShangApi.getBaseInfo(data)
+                .enqueue(new Callback<BaseResp>() {
+                    @Override
+                    public void onResponse(Call<BaseResp> call, Response<BaseResp> response) {
+                        if (response.isSuccessful()){
+                            final BaseResp resp = response.body();
+                            Log.d("RxAndroidAct",resp.getData().getSearchword());
+
+                            List<BaseResp.Result.Data> data1 = resp.getData().getResult().getData();
+                            if (data1.size() > 0){
+                                BaseResp.Result.Data baseData = data1.get(0);
+                                String pripId = baseData.getPripid();
+
+                                String page = "corp-query-entprise-info-primaryinfoapp-entbaseInfo-" + pripId + ".html";
+                                int nodeNum = baseData.getNodeNum();
+                                int entType = baseData.getEntType();
+                                gongShangApi.getDetailInfo(page,nodeNum,entType,"A")
+                                        .enqueue(new Callback<DetailResp>() {
+                                            @Override
+                                            public void onResponse(Call<DetailResp> call, Response<DetailResp> response) {
+                                                if (response.isSuccessful()){
+                                                    DetailResp detailResp = response.body();
+                                                    DetailResp.Result result = detailResp.getResult();
+
+                                                    Log.d("RxAndroidAct",result.getEntName());
+                                                }
+                                            }
+                                            @Override
+                                            public void onFailure(Call<DetailResp> call, Throwable t) {
+                                                t.printStackTrace();
+                                            }
+                                        });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<BaseResp> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+    }
 
     @Override
     protected void onDestroy() {
